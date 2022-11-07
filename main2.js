18819000ce83df4820f7f082904cf22b8ac3d1bf
6101020 <c6101020@planet.kanazawa-it.ac.jp>
2022-11-07 13:38:23 +0900

    add

--- main2.js
+++ main2.js
@@ -34,49 +34,24 @@ change_attr: function(stim){ // Change the content dynamically
 } 
 }
 
-const rect_object = {
-  obj_type: 'rect', // means a rectangle
-  startX: 0, // location in the canvas
-  startY: 0,
-  origin_center: true,
-  width: 100, // of the rectangle
-  height: 100,
-  line_color: 'rgb(0,0,0)',
-  fill_color: 'rgb(0,0,0)',
-  line_width: 4,
-  change_attr: function(stim, times, frames){ // called by the requestAnimationFrame
-      const frequency = 0.05;
-      const sin_value = Math.sin(2 * Math.PI * frequency * times/1000) // The times are in terms of milliseconds
-      const normalized_value = sin_value/2 + 1/2; // from 0 to 1
-      color = Math.floor(normalized_value * 255) // An integer between 0 and 255
-      const color_info = `rgb(${color}, ${color}, ${color})`
-
-      if (pixi_flag) { // Using PixiJS
-          const color_num = jsPsych.getCurrentTrial().getColorNum(color_info)
-          stim.pixi_obj.beginFill(color_num, 1);
-          stim.pixi_obj.drawRect(-stim.width/2, -stim.height/2, stim.width, stim.height);
-          stim.pixi_obj.endFill();
-      } else {
-          stim.fill_color = color_info
-      }
-  }
-}
+let elem = document.elementFromPoint(x, y);
 
 const line_object = {
   obj_type: 'line',
-  x1: 300,
-  y1: 200,
-  x2: 200,
-  y2: 400,
-  line_width: 6,
+  x1: 0,
+  y1: 0,
+  x2: window.innerWidth,
+  y2: window.innerHeight,
+  line_width: 2,
 }
 
 
 const trial = {
   type: jsPsychPsychophysics,
   pixi: pixi_flag,
-  stimuli: [text_object, rect_object, line_object],
+  stimuli: [text_object, line_object],
   choices: ['y', 'n'], // The participant can respond to the stimuli using the 'y' or 'n' key.
+  background_color: 'RGB = [255, 255, 255]',
   //canvas_width: 1600,
   //canvas_height: 600,
 }
