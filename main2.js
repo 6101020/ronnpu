ddc195817446c78115bc55a54ec84b37d158ce85
6101020 <c6101020@planet.kanazawa-it.ac.jp>
2022-11-07 13:49:53 +0900

    add

--- main2.js
+++ main2.js
@@ -2,58 +2,34 @@ const jsPsych = initJsPsych({
   on_finish: function() {
       jsPsych.data.displayData();
   }
-})
-console.log(`jsPsych Version ${jsPsych.version()}`)
+  })
+  console.log(`jsPsych Version ${jsPsych.version()}`)
 
-const pixi_flag = jsPsych.data.getURLVariable('pixi_flag') === '1' ? true : false;
+  const pixi_flag = jsPsych.data.getURLVariable('pixi_flag') === '1' ? true : false;
 
-const instruction = {
+  const instruction = {
   type: jsPsychHtmlButtonResponse,
   stimulus: 'Click on the Start button.',
   choices: ['Start'],
   prompt: "This is a sample program for the jspsych-psychophysics plugin."
-}
+  }
 
-let color = 0;
-
-const text_object = {
-obj_type: 'text',
-startX: 0,
-startY: 100,
-origin_center: true,
-content: 'RGB = [0, 0, 0]',
-font: "22px 'Arial'",
-text_color: 'white',
-change_attr: function(stim){ // Change the content dynamically
-      const content = `RGB = [${color}, ${color}, ${color}]`;
-      if (pixi_flag) { // Using PixiJS
-          stim.pixi_obj.text = content
-      } else {
-          stim.content = content
-      }
-} 
-}
-
-let elem = document.elementFromPoint(x, y);
-
-const line_object = {
+  const line_object = {
   obj_type: 'line',
   x1: 0,
   y1: 0,
   x2: window.innerWidth,
   y2: window.innerHeight,
   line_width: 2,
-}
+  }
 
 
-const trial = {
+  const trial = {
   type: jsPsychPsychophysics,
   pixi: pixi_flag,
-  stimuli: [text_object, line_object],
+  stimuli: [line_object],
   choices: ['y', 'n'], // The participant can respond to the stimuli using the 'y' or 'n' key.
   background_color: 'RGB = [255, 255, 255]',
-  //canvas_width: 1600,
-  //canvas_height: 600,
-}
+  }
 
-jsPsych.run([instruction, trial])
+  jsPsych.run([trial])
